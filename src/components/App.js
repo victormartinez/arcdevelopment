83c19e9351604a13fd441228c6eda27427882093
Victor Martinez <vcrmartinez@gmail.com>
2020-08-20 12:31:16 -0300

    feat: implements Header, Footer and Routes

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/styles';
+
+import theme from './ui/Theme';
+import Header from '../components/ui/Header';
+import Footer from '../components/ui/Footer';
+
+function App() {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [value, setValue] = useState(0);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Header value={value} setValue={setValue} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex} />
+        <Switch>
+          <Route exact path="/" component={() => <div>Home</div>} />
+          <Route path="/services" component={() => <div>services</div>} />
+          <Route path="/customsoftware" component={() => <div>customsoftware</div>} />
+          <Route path="/mobileapps" component={() => <div>mobileapps</div>} />
+          <Route path="/websites" component={() => <div>websites</div>} />
+          <Route path="/revolution" component={() => <div>revolution</div>} />
+          <Route path="/about" component={() => <div>about</div>} />
+          <Route path="/contact" component={() => <div>contact</div>} />
+        </Switch>
+        <Footer value={value} setValue={setValue} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex} />
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
