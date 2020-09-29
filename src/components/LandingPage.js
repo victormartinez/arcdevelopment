68c96f5af3c628b8cf66848b74be3412c32ac0de
Victor Martinez <vcrmartinez@gmail.com>
2020-09-29 19:13:05 -0300

    feat: add contact block

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -14,11 +14,13 @@ import customSoftwareIcon from '../assets/Custom Software Icon.svg';
 import mobileAppsIcon from '../assets/mobileIcon.svg';
 import websiteIcon from '../assets/websiteIcon.svg';
 import revolutionBackground from '../assets/repeatingBackground.svg';
+import infoBackground from '../assets/infoBackground.svg';
 
 export default function LandingPage() {
     const classes = useStyles();
     const theme = useTheme();
     const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
+    const matchesXS = useMediaQuery(theme.breakpoints.down("xs"));
 
     const defaultOptions = {
         loop: true,
@@ -139,6 +141,7 @@ export default function LandingPage() {
                 </Grid>
             </Grid>
             <Grid item>
+                {/*  ------ The Revolution Block ------- */}
                 <Grid container style={{ height: "100em", marginTop: "12em" }} alignItems="center" justify="center">
                     <Card className={classes.revolutionCard}>
                         <CardContent>
@@ -161,6 +164,42 @@ export default function LandingPage() {
                         </CardContent>
                     </Card>
                     <div className={classes.revolutionBackground} />
+                </Grid>
+            </Grid>
+            <Grid item>
+                {/*  ------ Information Block ------- */}
+                <Grid container style={{ height: "80em" }} spacing={matchesXS ? 10 : 0} direction="row" alignItems="center">
+                    <Grid item container style={{position: "absolute", textAlign: matchesXS ? "center" : "inherit"}} direction={ matchesXS ? "column" : "row"}>
+                        <Grid item sm style={{marginLeft: matchesXS ? 0 : matchesSM ? "2em" : "5em"}}>
+                            <Grid container direction="column">
+                                <Typography variant="h2" style={{ color: "white" }}>About Us</Typography>
+                                <Typography variant="subtitle2">Let's get personal.</Typography>
+                                <Grid item>
+                                    <Button variant="outlined" style={{ color: "white", borderColor: "white" }} className={classes.learnButtonHero}>
+                                        <span style={{marginRight: 10}}>Learn More</span>
+                                        <ButtonArrow width={15} height={15} fill="white" />
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid item sm style={{marginRight: matchesXS ? 0 : matchesSM ? "2em" : "5em", textAlign: matchesXS ? "center" : "right"}}>
+                            <Grid container direction="column">
+                                <Typography variant="h2" style={{ color: "white" }}>Contact Us</Typography>
+                                <Typography variant="subtitle2">
+                                    Say hello! 
+                                    <span role="img" aria-label="waving hand">👋</span>
+                                </Typography>
+                                <Grid item>
+                                    <Button variant="outlined" style={{ color: "white", borderColor: "white" }} className={classes.learnButtonHero}>
+                                        <span style={{marginRight: 10}}>Learn More</span>
+                                        <ButtonArrow width={15} height={15} fill="white" />
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    
+                    <div className={classes.infoBackground} />
                 </Grid>
             </Grid>
         </Grid>
@@ -262,5 +301,13 @@ const useStyles = makeStyles(theme => ({
             borderRadius: 0,
             width: "100%"
         }
+    },
+    infoBackground: {
+        backgroundImage: `url(${infoBackground})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        height: "100%",
+        width: "100%"
     }
 }))
